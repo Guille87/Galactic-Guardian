e327ad8b44ccca6dfef919f01d7afc217fe5dcb0
Guille87 <guillermo_amado@hotmail.es>
2026-09-21 02:15:26 +0200

    chore: pop de las cifras más largo y desde más pequeño (300 ms, 0,4)

--- src/core/settings.py
+++ src/core/settings.py
@@ -54,8 +54,8 @@ TEMBLOR_JEFE = 1.0          # el jefe cae
 # --- Cifras flotantes de daño ---
 CIFRA_DURACION_MS = 800     # lo que dura una cifra en pantalla
 CIFRA_VELOCIDAD = 80        # px/s que sube
-CIFRA_POP_MS = 150           # "pop" de entrada: nace pequeña, se pasa de tamaño y se asienta
-CIFRA_POP_INICIO = 0.5      # escala con la que nace
+CIFRA_POP_MS = 300          # "pop" de entrada: nace pequeña, se pasa de tamaño y se asienta
+CIFRA_POP_INICIO = 0.4      # escala con la que nace
 CIFRA_POP_MAX = 1.3         # escala máxima del pop
 CIFRA_TAMANO = 22           # tamaño de la fuente (px)
 CIFRA_TAMANO_NAVE = 28      # las que salen de la nave, algo mayores
